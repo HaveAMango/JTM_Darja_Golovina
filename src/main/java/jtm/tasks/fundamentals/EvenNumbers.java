b7e540e35523610171d145b0c13eec984ceb9697
haveamango <dagolovinad@gmail.com>
2020-01-20 18:39:53 +0200

    even numbers, max multiple

--- src/main/java/jtm/tasks/fundamentals/EvenNumbers.java
+++ src/main/java/jtm/tasks/fundamentals/EvenNumbers.java
@@ -4,29 +4,25 @@ import java.util.Arrays;
 
 public class EvenNumbers {
 
-    //TODO
-        /*
-            Complete the function which takes two arguments and returns all numbers which
-            are divisible by the given divisor.
-            First argument is an array of numbers and the second is the divisor.
-            Example:
-            divisibleBy([1, 2, 3, 4, 5, 6], 2) == [2, 4, 6]
-   
-         */
+	// TODO
+	/*
+	 * Complete the function which takes two arguments and returns all numbers which
+	 * are divisible by the given divisor. First argument is an array of numbers and
+	 * the second is the divisor. Example: divisibleBy([1, 2, 3, 4, 5, 6], 2) == [2,
+	 * 4, 6]
+	 * 
+	 */
 	public static int[] newNumbers;
-	
-    public static int[] divisibleBy(int[] numbers, int divider) {
-   
 
-    	for(int i = 0; i<numbers.length; i++ ) {
-    		for (int n = i+1; n<numbers.length; n++) {
-    			if (i>n && i%n ==0) {
-    				return newNumbers[numbers.length - i];
-    			}
-    		}
-    		
-    	}
-    	
-       
-    }
+	public static int[] divisibleBy(int[] numbers, int divider) {
+
+		int counter = 0;
+		for (int i = 0; i < numbers.length; i++) {
+			if (i % divider == 0) {
+				counter++;
+			}
+		}
+		int[] newNumbers = new int[counter];
+		return newNumbers;
+	}
 }
